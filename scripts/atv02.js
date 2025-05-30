f9fcfe270c870242a95555b8f7d7b9c843ac0d90
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 11:01:44 -0300

    feat: usando o push para adicionar os itens na lista

---
+++ scripts/atv02.js
@@ -0,0 +1,13 @@
+let cardapio = [
+  "pizza de calabresa",
+  "lasanha",
+  "sushi",
+  "salada Caesar",
+  "pastel",
+  "yakisoba",
+  "feijoada",
+];
+console.log(cardapio);
+
+cardapio.push("hambúrguer artesanal", "wrap de frango");
+console.log(cardapio);
