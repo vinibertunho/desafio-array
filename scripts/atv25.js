68ce022f3e04dfcbf354acdb2e5766108572c140
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 15:49:35 -0300

    feat: use o reverse para inverter a ordem da lista

---
+++ scripts/atv25.js
@@ -0,0 +1,14 @@
+let atualizacoes = [
+    "Backup diário concluído",
+    "Servidor reiniciado",
+    "Nova versão implantada",
+    "Logs exportados",
+    "Firewall atualizado",
+    "Monitoramento ativado",
+    "Banco de dados otimizado",
+    "Script de segurança executado"
+  ];
+
+  let atualizacoesinvertida = atualizacoes.reverse()
+  console.log(atualizacoesinvertida);
+  
