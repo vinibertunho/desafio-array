ac56de8d53593898b953140faf35160ca0f5d326
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 10:59:11 -0300

    feat: usa o push para adicionar os itens na lista

---
+++ scripts/atv01.js
@@ -0,0 +1,13 @@
+let promocoes = [
+  "teclado mecânico",
+  "mouse gamer",
+  "monitor ultrawide",
+  "cadeira ergonômica",
+  "webcam HD",
+  "fone bluetooth",
+  "notebook i5",
+  "hub USB",
+];
+console.log(promocoes)
+promocoes.push("SSD 1TB", "mesa digitalizadora");
+console.log(promocoes);
