4d357ab6428c91f4d9e3ffe04917105b2f22d85e
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 14:58:16 -0300

    feat: use o concat para juntar as listas

---
+++ scripts/atv18.js
@@ -0,0 +1,15 @@
+let hardware = ["processador i7", "placa-mãe", "memória RAM 16GB", "SSD 1TB"];
+
+let perifericos = [
+  "teclado gamer",
+  "mouse ótico",
+  "webcam HD",
+  "fone com microfone",
+];
+
+console.log(hardware);
+console.log(perifericos);
+
+let itensCombinados = hardware.concat(perifericos);
+
+console.log(itensCombinados);
---
+++ scripts/atv19.js
@@ -0,0 +1,16 @@
+let codigos = [
+    "abc123",
+    "def456",
+    "ghi789",
+    "jkl012",
+    "mno345",
+    "pqr678",
+    "stu901",
+    "vwx234"
+  ];
+console.log(codigos);
+
+let maiuscula = codigos.map(codigos => codigos.toUpperCase())
+console.log(maiuscula);
+
+  
