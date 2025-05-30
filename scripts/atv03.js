22ce8ac5c6b1029717e814c8d015a138f610aca9
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 11:05:10 -0300

    feat: use o pop para remover o ultimo item da lista

---
+++ scripts/atv03.js
@@ -0,0 +1,15 @@
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+];
+console.log(producao);
+
+let removendoUltumoItem = producao.pop()
+
+console.log(producao);
+console.log('o ultimo item removido foi:', removendoUltumoItem);
+
