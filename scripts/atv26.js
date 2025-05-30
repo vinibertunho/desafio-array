bad18e91dd488f8e35b2701ec34973ce4e3c6541
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 15:50:59 -0300

    feat: use o reverse para inverter a lista

---
+++ scripts/atv26.js
@@ -0,0 +1,14 @@
+let historico = [
+    "login",
+    "painel",
+    "relatórios",
+    "detalhes do cliente",
+    "editar dados",
+    "configurações",
+    "logout"
+  ];
+
+  let historicoInvertido = historico.reverse()
+
+  console.log(historicoInvertido);
+  
