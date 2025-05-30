8ac8253ff06e010fb6200db2f67f67db7af93d8c
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 11:36:35 -0300

    feat: usa o unshift para adionar itens no inicio da lista

---
+++ scripts/atv08.js
@@ -0,0 +1,13 @@
+let chamada = [
+  "Carlos",
+  "Fernanda",
+  "João",
+  "Mariana",
+  "Lucas",
+  "Camila"
+];
+
+// Adicionando as alunas no início da lista
+chamada.unshift("Beatriz", "Rafaela");
+
+console.log(chamada);
