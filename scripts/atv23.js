02c08d22c1c3f61377cb3b2cb10f6e7ceab68786
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 15:41:48 -0300

    feat: use o sort para ordenar os itens

---
+++ scripts/atv23.js
@@ -0,0 +1,14 @@
+let candidatos = [
+    "Marcos",
+    "Aline",
+    "Carla",
+    "João",
+    "Fernanda",
+    "Lucas",
+    "Bruno",
+    "Isabela"
+]
+
+let NomesOrdemAlfabetica = candidatos.sort()
+  console.log(NomesOrdemAlfabetica);
+  
