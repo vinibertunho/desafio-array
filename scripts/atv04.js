77f2b356c56dfff93aaf235588e6109ceeb23e20
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 11:09:31 -0300

    feat: use o pop para remover o ultimo interessado

--- scripts/atv04.js
+++ scripts/atv04.js
@@ -1,5 +1,6 @@
-let inscritos = [
-  "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
-];
-console.log(inscritos)
+let inscritos = ["Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"];
+console.log(inscritos);
 
+let removendoUltimoInteressado = inscritos.pop();
+console.log(inscritos);
+console.log("o ultimo interessado removido foi:", removendoUltimoInteressado);
