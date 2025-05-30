3f5bcfb458cbd827f3c17c26834d9617153f28dc
ViniBertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 11:34:17 -0300

    feat: usa o splice para adicionar item na lista

--- scripts/atv07.js
+++ scripts/atv07.js
@@ -6,7 +6,7 @@ let filaAtendimento = [
   "cliente 007"
 ];
 console.log(filaAtendimento);
-
+// usando o splice para adicionar os clientes na lista
 let clientesAdicionados =  filaAtendimento.splice(0, 1, "cliente 001" , "cliente 002")
 console.log(filaAtendimento);
 
