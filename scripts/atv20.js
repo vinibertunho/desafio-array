fd85a228b4781ea3756a1ae16cfebb62104d10c7
vinibertunho <vinicius.m.silva36@aluno.senai.br>
2025-05-30 15:02:23 -0300

    feat: use o map para colocar os preços em reais

--- scripts/atv20.js
+++ scripts/atv20.js
@@ -1,1 +1,16 @@
-    
+let precos = [
+    129.9,
+    399.5,
+    259.0,
+    89.9,
+    599.99,
+    39.5,
+    999.0
+  ];
+  
+  // Nova lista com os preços formatados em moeda brasileira (BRL)
+  let precosFormatados = precos.map(preco =>
+    preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+  );
+  
+  console.log(precosFormatados);
